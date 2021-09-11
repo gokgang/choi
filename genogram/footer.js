e653128b6940e9ba02bad1d9d7daac991418abb4
gokgang <68494728+gokgang@users.noreply.github.com>
2021-09-11 11:15:06 +0900

    Add files via upload

---
+++ genogram/footer.js
@@ -0,0 +1,11 @@
+document.write(
+  `
+    <tr style="height: 150px;">
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+  </body>
+
+</html>
+`
+);
---
+++ genogram/g300deajun.js
@@ -0,0 +1,338 @@
+document.write(`
+<body>
+  <h1><a href="./default.html">곡강최가 가계도</a></h1>
+  <hr>
+  <table>
+    <tr>
+      <td>시조</td>
+      <td>호<br>湖</td>
+    </tr>
+
+    <tr>
+      <td>&nbsp;</td>
+      <td class="right">├───</td>
+      <td>───┬───</td>
+      <td>───┬───</td>
+      <td class="left">───┐</td>
+    </tr>
+
+    <tr>
+      <td>2세</td>
+      <td>근<br>瑾</td>
+      <td>영<br>瑛</td>
+      <td>박<br>樸</td>
+      <td>경<br>瓊</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>3세</td>
+      <td colspan="1"></td>
+      <td>탁<br>琸</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+    <tr>
+      <td>4세</td>
+      <td colspan="1"></td>
+      <td>진헌<br>榛軒</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>5세</td>
+      <td colspan="1"></td>
+      <td>우종<br>佑鍾</td>
+    </tr>
+
+    <tr>
+      <td colspan="2">
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>6세</td>
+      <td colspan="1"></td>
+      <td>청<br>聽</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>7세</td>
+      <td colspan="1"></td>
+      <td>운성<br>韻盛</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+    <tr>
+      <td>8세</td>
+      <td colspan="1"></td>
+      <td>경어<br>敬語</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>9세</td>
+      <td colspan="1"></td>
+      <td>원묵<br>願默</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>10세</td>
+      <td colspan="1"></td>
+      <td>헌진<br>獻振</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>11세</td>
+      <td colspan="1"></td>
+      <td>경운<br>慶雲</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>12세</td>
+      <td colspan="1"></td>
+      <td>불수<br>佛壽</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>13세</td>
+      <td colspan="1"></td>
+      <td>홍<br>洪</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>14세</td>
+      <td colspan="1"></td>
+      <td>저<br>楮</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>15세</td>
+      <td colspan="1"></td>
+      <td>점<br>霑</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>16세</td>
+      <td colspan="1"></td>
+      <td>운로<br>雲露</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td class="right">├───</td>
+      <td>───────</td>
+      <td class="left">───┐</td>
+    </tr>
+
+    <tr>
+      <td>17세</td>
+      <td colspan="1"></td>
+      <td>경인<br>慶仁</td>
+      <td colspan="1">&nbsp;</td>
+      <td id="경화">경화<br>慶華</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td class="right">├───</td>
+      <td class="left">───┐</td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>18세</td>
+      <td colspan="1"></td>
+      <td>대연<br>大演</td>
+      <td>대윤<br>大潤</td>
+      <td id="대준">대준<br>大俊</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td><a href="./g001.html#대연">▼</a></td>
+      <td><a href="./g200.html#대윤">▼</a></td>
+      <td class="right">├───</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td class="left">───┐</td>
+    </tr>
+
+    <tr>
+      <td>19세</td>
+      <td colspan="3"></td>
+      <td>여성<br>汝成</td>
+      <td colspan="10"></td>
+      <td>지원<br>之元</td>
+    </tr>
+
+    <tr>
+      <td colspan="4"></td>
+      <td class="right">├───</td>
+      <td>───┬───</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td class="left">───┐</td>
+      <td colspan="1"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>20세</td>
+      <td colspan="3"></td>
+      <td>만장<br>萬章</td>
+      <td>만중<br>萬重</td>
+      <td colspan="7"></td>
+      <td>만익<br>萬益</td>
+      <td colspan="1"></td>
+      <td>억기<br>億紀</td>
+    </tr>
+
+    <tr>
+      <td colspan="5"></td>
+      <td>│</td>
+      <td colspan="7"></td>
+      <td class="right">├───</td>
+      <td class="left">───┐</td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>21세</td>
+      <td colspan="4"></td>
+      <td>덕륜<br>德崙</td>
+      <td colspan="7"></td>
+      <td>덕노<br>德老</td>
+      <td>태노<br>台老</td>
+      <td>귀수<br>龜壽</td>
+    </tr>
+
+    <tr>
+      <td colspan="5"></td>
+      <td class="right">├───</td>
+      <td class="left">───┐</td>
+      <td colspan="6"></td>
+      <td>△</td>
+      <td>△</td>
+      <td>△</td>
+    </tr>
+
+    <tr>
+      <td>22세</td>
+      <td colspan="4"></td>
+      <td>일경<br>日鏡</td>
+      <td>일신<br>日新</td>
+    </tr>
+
+    <tr>
+      <td colspan="5"></td>
+      <td>│</td>
+      <td class="right">├───</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td class="left">───┐</td>
+    </tr>
+
+    <tr>
+      <td>23세</td>
+      <td colspan="4"></td>
+      <td>용기<br>龍起</td>
+      <td>종헌<br>宗憲</td>
+      <td colspan="5"></td>
+      <td>종철<br>宗哲</td>
+    </tr>
+
+    <tr>
+      <td colspan="5"></td>
+      <td>△</td>
+      <td class="right">├───</td>
+      <td class="left">───┐</td>
+      <td colspan="4"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>24세</td>
+      <td colspan="5"></td>
+      <td id="건극">건극<br>建極</td>
+      <td>건곡<br>建鵠</td>
+      <td colspan="4"></td>
+      <td id="건곡">건곡<br>建鵠</td>
+    </tr>
+
+    </tr>
+`);
