75064f1374749556841a397217064a9b444fca86
gokgang <68494728+gokgang@users.noreply.github.com>
2021-10-26 17:39:42 +0900

    Add files via upload

---
+++ genogram/geno200deayun.js
@@ -0,0 +1,253 @@
+document.write(`
+  <h1><a href="./default.html">곡강최가 가계도</a></h1>
+  <hr>
+  <table>
+    <tr>
+      <td>시조</td>
+      <td>호<br>湖</td>
+    </tr>
+
+    <tr>
+      <td>&nbsp;</td>
+      <td class="right">├──</td>
+      <td>───┬───</td>
+      <td>───┬───</td>
+      <td class="left">───┐</td>
+    </tr>
+
+    <tr>
+      <td>2세</td>
+      <td>근<br>瑾</td>
+      <td>영<br>瑛</td>
+      <td>박<br>樸</td>
+      <td>경<br>瓊</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>3세</td>
+      <td colspan="1"></td>
+      <td>탁<br>琸</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+    <tr>
+      <td>4세</td>
+      <td colspan="1"></td>
+      <td>진헌<br>榛軒</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>5세</td>
+      <td colspan="1"></td>
+      <td>우종<br>佑鍾</td>
+    </tr>
+
+    <tr>
+      <td colspan="2">
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>6세</td>
+      <td colspan="1"></td>
+      <td>청<br>聽</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>7세</td>
+      <td colspan="1"></td>
+      <td>운성<br>韻盛</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+    <tr>
+      <td>8세</td>
+      <td colspan="1"></td>
+      <td>경어<br>敬語</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>9세</td>
+      <td colspan="1"></td>
+      <td>원묵<br>願默</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>10세</td>
+      <td colspan="1"></td>
+      <td>헌진<br>獻振</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>11세</td>
+      <td colspan="1"></td>
+      <td>경운<br>慶雲</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>12세</td>
+      <td colspan="1"></td>
+      <td>불수<br>佛壽</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>13세</td>
+      <td colspan="1"></td>
+      <td>홍<br>洪</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>14세</td>
+      <td colspan="1"></td>
+      <td>저<br>楮</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>15세</td>
+      <td colspan="1"></td>
+      <td>점<br>霑</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td>│</td>
+    </tr>
+
+    <tr>
+      <td>16세</td>
+      <td colspan="1"></td>
+      <td>운로<br>雲露</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td class="right">├───</td>
+      <td>───────</td>
+      <td class="left">───┐</td>
+    </tr>
+
+    <tr>
+      <td>17세</td>
+      <td colspan="1"></td>
+      <td>경인<br>慶仁</td>
+      <td colspan="1">&nbsp;</td>
+      <td>경화<br>慶華</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td class="right">├───</td>
+      <td class="left">───┐</td>
+      <td><a href="./g300.html">▼</a></td>
+    </tr>
+
+    <tr>
+      <td>18세</td>
+      <td colspan="1"></td>
+      <td>대연<br>大演</td>
+      <td id="대윤">대윤<br>大潤</td>
+    </tr>
+
+    <tr>
+      <td colspan="2"></td>
+      <td><a href="./g001.html">▼</a></td>
+      <td class="right">├───</td>
+      <td>───┬───</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───────</td>
+      <td>───┬───</td>
+      <td>───┬───</td>
+      <td class="left">──┐</td>
+    </tr>
+
+    <tr>
+      <td>19세</td>
+      <td colspan="2"></td>
+      <td id="유항">유항<br>有恒</td>
+      <td id="유보">유보<br>有輔</td>
+      <td colspan="4"></td>
+      <td id="남두">남두<br>南斗</td>
+      <td>유진<br>有軫</td>
+      <td>유장<br>有章</td>
+    </tr>
+
+    <tr>
+      <td colspan="3"></td>
+      <td><a href="./g200.html#유항">▼</a></td>
+      <td class="right">├───</td>
+      <td>───┬───</td>
+      <td>───┬───</td>
+      <td>───┬───</td>
+      <td class="left">───┐</td>
+      <td><a href="./g200.html#남두">▼</a></td>
+      <td><a href="./g221.html#유진">▼</a></td>
+      <td><a href="./g270.html#유장">▼</a></td>
+    <tr>
+      <td>20세</td>
+      <td colspan="3"></td>
+      <td>만봉<br>萬峰</td>
+      <td>만리<br>萬里</td>
+      <td>만현<br>萬賢</td>
+      <td>만상<br>萬相</td>
+      <td>만재<br>萬載</td>
+    </tr>
+
+`);
