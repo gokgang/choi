6d8ae4dd6517070330c784d99e243216e0a37d22
gokgang <68494728+gokgang@users.noreply.github.com>
2023-11-17 10:35:11 +0900

    Delete web directory

--- web/css/footer.js
+++
@@ -1,7 +0,0 @@
-document.write(
-  `
-  <p style="text-align: right;">
-    <img src="./img_flower.png" style="width: 50%;opacity: 0.3;">
-  </p>
-  `
-);
--- web/script/foot.js
+++
@@ -1,8 +0,0 @@
-document.write(
-  `
-  <div style="display:inline-block; position: fixed; right: 0; bottom: 10%; font-size: 3rem;"><a href="#top" style="color: green;">⇧</a></div>
-  <p style="text-align: right;">
-  <img src="./img_flower.png" style="width: 50%;opacity: 0.3;">
-</p>
-  `
-);
